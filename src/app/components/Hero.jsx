c62896449cfb99e034cbecd11a9c1be6d0409bca
adisehdev <sehdevaditya@gmail.com>
2025-01-27 11:48:33 +0530

    adding sample images to home page

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -3,9 +3,11 @@ import React, { useState } from 'react'
 import Lookup from '../_data/Lookup'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Hero = () => {
     const [logoTitle,setLogoTitle] = useState('')
+    const sampleImages = ['/sample_1.png','/sample_2.png','/sample_3.png','/sample_4.png','/sample_5.png','/sample_6.png']
   return (
 
     <div className='flex flex-col items-center mt-24 gap-5'>
@@ -17,6 +19,16 @@ const Hero = () => {
             <Link href={`/create?title=${logoTitle}`}>
             <Button className='w-full p-6 font-bold'>Get Started</Button>
             </Link>
+        </div>
+
+        <div className='my-10 grid grid-cols-2 md:grid-cols-3 gap-10'>
+            {
+                sampleImages.map((img,index) => (
+                    <div key={index}>
+                        <Image src={img} alt={img} width={300} height={200} className='w-full rounded-xl h-[200px] object-cover'/>
+                    </div>
+                ))
+            }
         </div>
     </div>
   )
